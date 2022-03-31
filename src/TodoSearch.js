e3b1529be1427cda2f04fba0a4869e8474dd8060
SpidySamurai <javierchiortiz@gmail.com>
2022-03-31 15:24:10 -0600

    TODO search component

---
+++ src/TodoSearch.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./TodoSearch.css";
+
+function TodoSearch({ searchValue, setSearchValue }) {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+  return (
+    <section className="TodoSearch">
+      <input
+        className="TodoSearch__bar"
+        placeholder="Search a TODO"
+        value={searchValue}
+        onChange={onSearchValueChange}
+      ></input>
+      {/* <button className="TodoSearch__button" ></button> */}
+    </section>
+  );
+}
+
+export { TodoSearch };
