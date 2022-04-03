62b15d58fec9caead17b0d46d3340a8a5422f1cf
SpidySamurai <javierchiortiz@gmail.com>
2022-04-03 00:31:01 -0600

    Add TodoContext props

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./TodoCounter.css";
+import { TodoContext } from "../TodoContext";
 
-function TodoCounter({ totalTodos, completedTodos }) {
+function TodoCounter() {
+  const { completedTodos, totalTodos } = React.useContext(TodoContext);
   return (
     <header className="TodoCounter">
       <h1>TODO APP</h1>
