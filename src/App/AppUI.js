d37f6a331dcb10611af74cda234796a784cd9cf9
SpidySamurai <javierchiortiz@gmail.com>
2022-04-02 23:04:50 -0600

    Clean APPUI and Index

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,41 +1,46 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
-function AppUI({
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  toggleCompleteTodo,
-  deleteTodo,
-}) {
+function AppUI({}) {
   return (
     <React.Fragment>
       <main>
-        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoCounter />
+        <TodoSearch />
 
-        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-        <TodoList>
-          {searchedTodos.map((todo) => (
-            <TodoItem
-              key={todo.text}
-              text={todo.text}
-              completed={todo.completed}
-              onToggleComplete={() => {
-                toggleCompleteTodo(todo.text);
-              }}
-              onDelete={() => {
-                deleteTodo(todo.text);
-              }}
-            />
-          ))}
-        </TodoList>
+        <TodoContext.Consumer>
+          {({
+            error,
+            loading,
+            searchedTodos,
+            toggleCompleteTodo,
+            deleteTodo,
+          }) => (
+            <TodoList>
+              {error && <p>Hubo un error...</p>}
+              {loading && <p>Cargando los todos!</p>}
+              {!loading && !searchedTodos.lenght && <p>Crea tu primer todo!</p>}
+              {searchedTodos.map((todo) => (
+                <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}
+                  onToggleComplete={() => {
+                    toggleCompleteTodo(todo.text);
+                  }}
+                  onDelete={() => {
+                    deleteTodo(todo.text);
+                  }}
+                />
+              ))}
+            </TodoList>
+          )}
+        </TodoContext.Consumer>
 
         <CreateTodoButton />
       </main>
