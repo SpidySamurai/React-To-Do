ec3ca7e72c97135072c89dc8911fcae7ec00235c
SpidySamurai <javierchiortiz@gmail.com>
2022-04-21 18:53:32 -0500

    Todo fix error when cleaning all todos

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -3,8 +3,13 @@ import "./TodoList.css";
 
 function TodoList(props) {
   return (
-    <section>
-      <ul>{props.children}</ul>
+    <section className="TodoList-container">
+      <ul>
+        {props.error && props.onError()}
+        {props.loading && props.onLoading()}
+        {/* {!props.loading && !props.searchedTodos.length && props.onEmptyTodos} */}
+        {props.searchedTodos.map(props.render)}
+      </ul>
     </section>
   );
 }
